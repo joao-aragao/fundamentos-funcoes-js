d16ba364630dde0aea295591fdb26e56165cd73b
Joao Aragao <joaovictoraragaoads@gmail.com>
2021-03-03 22:46:57 -0300

    Tipos de funções

--- foudation/anatomy-1.js
+++ foudation/anatomy-1.js
@@ -5,3 +5,27 @@ function sayHello() {
 };
 
 sayHello();
+
+/* Template String */
+/* Função com parâmetro */
+function sayHelloTo(name) {
+    console.log(`Hello ${name}`)
+}
+
+sayHelloTo('Dex');
+
+/* Função com retorno, mas sem parâmetro */
+function returnHi() {
+    return 'Hi'
+}
+
+const greeting = returnHi()
+console.log(greeting)
+console.log(returnHi())
+
+/* Função com parâmetro e retorno */
+function returnHiTo(name) {
+    return `Hi ${name}!`
+}
+
+console.log(returnHiTo('Bitora'))
